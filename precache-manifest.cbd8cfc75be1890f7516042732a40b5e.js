e3c1c0204b0a7996c7266a9f758b8cf4924de92f
Nazar <yanchishin1@gmail.com>
2020-05-10 15:39:01 +0300

    Updates

---
+++ precache-manifest.cbd8cfc75be1890f7516042732a40b5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9d0af2bf68e0fe03ff8f73d6838e8e",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/index.html"
+  },
+  {
+    "revision": "78232eee6c49dd89be40",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/static/css/main.da705ce2.chunk.css"
+  },
+  {
+    "revision": "6378024e0f0ce30ffe96",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/static/js/2.1b3bf2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/static/js/2.1b3bf2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78232eee6c49dd89be40",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/static/js/main.b54f2f8c.chunk.js"
+  },
+  {
+    "revision": "1929910dad459d325f08",
+    "url": "/http;//NazarYanchishin.github.io/React_hw1/static/js/runtime-main.0673d066.js"
+  }
+]);
